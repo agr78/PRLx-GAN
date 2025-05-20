56a7996c42e5b3d607128d6b4200259167dac01d
agr78 <agr78@cornell.edu>
2025-05-20 12:21:16 -0400

    Update `make_env.sh`

--- src/make_env.sh
+++ src/make_env.sh
@@ -14,15 +14,3 @@ conda install click requests tqdm pyspng ninja imageio-ffmpeg==0.4.3
 conda install -c conda-forge cudatoolkit-dev -y
 # Clone StyleGAN2-ADA
 git clone https://github.com/NVlabs/stylegan2-ada-pytorch.git
-# Test the repo
-python generate.py \ 
---outdir=out --trunc=1 --seeds=85,265,297,849 \
---network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
-# Retrain (optional)
-python train.py --outdir=./training-runs --data=/your/QSM/rims --gpus=8
-# Generate synthetic PRL QSM
-python generate.py --outdir=./out --trunc=1 --seeds=100-200 \ 
---network=./training-runs/00020--auto8/network-snapshot-025000.pkl 
-# Project noisy rims onto latent space
-python projector.py --outdir=out --target=/sample_noisy_rim.png \ 
---network=./training-runs/00020--auto8/network-snapshot-025000.pkl 

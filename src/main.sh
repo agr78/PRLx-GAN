0b29b367ca25645a2e8232f793c452a60e0a338a
agr78 <agr78@cornell.edu>
2025-05-28 13:13:32 -0400

    Update `main.sh`

--- src/main.sh
+++ src/main.sh
@@ -9,7 +9,7 @@ conda activate rimgan
 echo Installing some extra packages
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
 # Install some additional packages
-conda install click requests tqdm ninja imageio psutil
+conda install click requests tqdm ninja imageio psutil scipy
 # Install some package from pip
 ~/anaconda3/envs/rimgan/bin/pip install pyspng
 ~/anaconda3/envs/rimgan/bin/pip install imageio-ffmpeg==0.4.3
@@ -23,12 +23,10 @@ cd stylegan2-ada-pytorch
 # Test the repo
 python generate.py --outdir=out --trunc=1 --seeds=85,265,297,849 --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
 # Apply patch
-git apply ./patch/prlx_gan.patch
+git apply ~PRLx-GAN/patch/prlx_gan.patch
 # Retrain (optional)
 python train.py --outdir=./training-runs --data=/your/QSM/rims --gpus=8
 # Generate synthetic PRL QSM
-python generate.py --outdir=./out --trunc=1 --seeds=100-200 \ 
---network=./training-runs/00020--auto8/network-snapshot-025000.pkl 
+python generate.py --outdir=./out --trunc=1 --seeds=100-200 --network=~PRLx-GAN/net/network-snapshot-025000.pkl 
 # Project noisy rims onto latent space
-python projector.py --outdir=out --target=/sample_noisy_rim.png \ 
---network=./training-runs/00020--auto8/network-snapshot-025000.pkl 
+python projector.py --outdir=out --target=/sample_noisy_rim.png --network=~/PRLx-GAN/net/network-snapshot-025000.pkl 

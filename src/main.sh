4d6d102697cfe01ef5e4becc88aeddebc49b9d39
agr78 <agr78@cornell.edu>
2025-05-28 12:52:52 -0400

    Update `main.sh`

--- src/main.sh
+++ src/main.sh
@@ -9,7 +9,7 @@ conda activate rimgan
 echo Installing some extra packages
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
 # Install some additional packages
-conda install click requests tqdm pyspng ninja imageio-ffmpeg==0.4.3
+conda install click requests tqdm ninja
 # Install some package from pip
 ~/anaconda3/envs/rimgan/bin/pip install pyspng
 ~/anaconda3/envs/rimgan/bin/pip install imageio-ffmpeg==0.4.3

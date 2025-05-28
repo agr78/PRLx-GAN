d5299b4681d190351ddd6612c9bd6807ac7b538e
agr78 <agr78@cornell.edu>
2025-05-28 12:44:30 -0400

    Update `main.sh`

--- src/main.sh
+++ src/main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Make environment
+echo Creating and activating environment
 conda config --add channels defaults
 conda create -n "rimgan" python=3.7.0 ipython
 # Activate it
 conda activate rimgan
 # Install PyTorch
+echo Installing some extra packages
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
 # Install some additional packages
 conda install click requests tqdm pyspng ninja imageio-ffmpeg==0.4.3
@@ -14,7 +16,10 @@ conda install click requests tqdm pyspng ninja imageio-ffmpeg==0.4.3
 # Optionally install the CUDA development toolkit package
 conda install -c conda-forge cudatoolkit-dev -y
 # Clone StyleGAN2-ADA
+cd
+echo Cloning StyleGAN2-ADA to home directory
 git clone https://github.com/NVlabs/stylegan2-ada-pytorch.git
+cd stylegan2-ada-pytorch
 # Test the repo
 python generate.py \ 
 --outdir=out --trunc=1 --seeds=85,265,297,849 \

c69e73fa7819fabff7670a9d8f90d690f2ee333e
agr78 <agr78@cornell.edu>
2025-05-28 14:13:48 -0400

    Update `main.sh`

--- src/main.sh
+++ src/main.sh
@@ -25,10 +25,12 @@ python generate.py --outdir=out --trunc=1 --seeds=85,265,297,849 --network=https
 # Apply patch
 git apply ~/PRLx-GAN/patch/prlx_gan.patch
 # Retrain (optional)
-python train.py --outdir=./training-runs --data=/your/QSM/rims --gpus=8
+python train.py --outdir=./training-runs --data=/your/QSM/rims --gpus=8 || echo Specify a valid directory to retrain
 cd ../PRLx-GAN
 # Generate synthetic PRL QSM
+echo Generating synthetic rims
 python ~/stylegan2-ada-pytorch/generate.py --outdir=./out --trunc=1 --seeds=100-200 --network=./net/network-snapshot-025000.pkl 
 # Project noisy rims onto latent space
-mkdir ~/PRLx-GAN/proj
+echo Denoising ambiguous rim via latent projection
 python ~/stylegan2-ada-pytorch/projector.py --outdir=out --target=./sample/noisy_rim.png --network=./net/network-snapshot-025000.pkl 
+echo Done, see ~/PRLx-GAN/out/ for generated and denoised samples

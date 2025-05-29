4635a510229e7663c1e2dfe45ab6b8b36f7549ba
Alexandra G. Roberts <69256818+agr78@users.noreply.github.com>
2025-05-29 06:11:09 -0400

    Update `make_env.sh`

--- src/make_env.sh
+++ src/make_env.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 # Make environment
-conda create -n "rimgan" python=3.7.0 ipython
+echo Creating and activating environment
+conda config --add channels defaults
+conda create -n "rimgan" python=3.7.0 ipython -y
 # Activate it
 conda activate rimgan
 # Install PyTorch
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
+echo Installing some extra packages
+conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch -y
 # Install some additional packages
-conda install click requests tqdm pyspng ninja imageio-ffmpeg==0.4.3
+conda install click requests tqdm ninja imageio psutil scipy -y
 # Install some package from pip
-~/anaconda3/envs/rimgan/bin/pip install pyspng
-~/anaconda3/envs/rimgan/bin/pip install imageio-ffmpeg==0.4.3
+$CONDA_PREFIX/bin/pip install pyspng
+$CONDA_PREFIX/bin/pip install imageio-ffmpeg==0.4.3
 # Optionally install the CUDA development toolkit package
 conda install -c conda-forge cudatoolkit-dev -y
 # Clone StyleGAN2-ADA
+cd
+echo Cloning StyleGAN2-ADA to home directory
 git clone https://github.com/NVlabs/stylegan2-ada-pytorch.git
+cd stylegan2-ada-pytorch
+# Test the repo
+python generate.py --outdir=out --trunc=1 --seeds=85,265,297,849 --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+# Apply patch
+git apply ~/PRLx-GAN/patch/prlx_gan.patch

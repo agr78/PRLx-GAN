1270175ca46a464c6dbae280e02ba94e193b9e97
Alexandra G. Roberts <69256818+agr78@users.noreply.github.com>
2025-05-29 06:13:12 -0400

    Update `denoise.sh`

--- src/denoise.sh
+++ src/denoise.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Retrain (optional)
-python train.py --outdir=./training-runs --data=/your/QSM/rims --gpus=8
+python train.py --outdir=./training-runs --data=/your/QSM/rims --gpus=8 || echo Specify a valid directory to retrain
+cd ../PRLx-GAN
 # Generate synthetic PRL QSM
-python generate.py --outdir=./out --trunc=1 --seeds=100-200 \ 
---network=./training-runs/00020--auto8/network-snapshot-025000.pkl 
+echo Generating synthetic rims
+python ~/stylegan2-ada-pytorch/generate.py --outdir=./out --trunc=1 --seeds=100-200 --network=./net/network-snapshot-025000.pkl 
 # Project noisy rims onto latent space
-python projector.py --outdir=out --target=/sample_noisy_rim.png \ 
---network=./training-runs/00020--auto8/network-snapshot-025000.pkl 
+echo Denoising ambiguous rim via latent projection
+python ~/stylegan2-ada-pytorch/projector.py --outdir=out --target=./sample/noisy_rim.png --network=./net/network-snapshot-025000.pkl 
+echo Done, see ~/PRLx-GAN/out/ for generated and denoised samples
